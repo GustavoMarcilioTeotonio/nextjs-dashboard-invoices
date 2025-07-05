4dc7eea41fecc60f6fce18d1d1565ba3cd49198a
Gustavo Marcilio Teotonio <gmarcilioteotonio@gmail.com>
2025-07-04 21:07:33 -0300

    push

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,12 @@
 import {lusitana} from "@/app/ui/fonts";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 import {revenue} from "../lib/placeholder-data"
+import { fetchLatestInvoices, fetchRevenue } from "../lib/data";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
 export default async function Page(){
+    const revenue = await fetchRevenue(); //busca as receitas
+    const latestInvoices = await fetchLatestInvoices(); 
+    //busca as últimas faturas 
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md: text-2x1`}>
@@ -14,6 +19,12 @@ export default async function Page(){
                 <RevenueChart revenue={revenue} />
 
             </div>
+
+            <div className="mt-6 grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                <LatestInvoices latestInvoices={latestInvoices} />
+                
+            </div>
+
         </main>
     )
 }
